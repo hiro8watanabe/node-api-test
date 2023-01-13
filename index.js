eaaacc23aa76ae2478f5c7e0f65f3f5f6dabdba2
hiro <onecast.onelost615@gmail.com>
2023-01-14 05:00:44 +0900

    customers情報追加

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ app.use(express.json());
 
 app.listen(PORT, () => console.log("サーバーが起動しました"));
 
+app.get("/", (req, res) => {
+  res.send("send");
+});
+
+const customers = [
+  { name: "田中", id: 1 },
+  { name: "山田", id: 2 },
+  { name: "武田", id: 3 },
+  { name: "橋本", id: 4 },
+  { name: "高橋", id: 5 },
+];
+
+app.get("/api/customers", (req, res) => {
+  res.send(customers);
+});
+
